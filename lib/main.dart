b6363da8144f793950415c915a7ff4c359b68327
Volker Leck <volker.leck@gmail.com>
2017-10-30 22:12:28 +0100

    add counter and timer for clock increment

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 void main() {
   runApp(new MyApp());
@@ -6,40 +7,22 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
           primarySwatch: Colors.red,
           brightness: Brightness.light,
           canvasColor: Colors.yellow
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(title: 'Mines'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -48,6 +31,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  void _startTimer() {
+    new Timer.periodic(new Duration(seconds: 1), _incTimer);
+    _isRunning = true;
+  }
+
+  void _incTimer(Timer timer) {
+    setState(() {
+      _seconds++;
+    });
+  }
 
   List<List<int>> _state = [
     [10, 10, 10, 10, 10, 10, 10, 10],
@@ -62,45 +56,38 @@ class _MyHomePageState extends State<MyHomePage> {
     [10, 10, 10, 10, 10, 10, 10, 10],
   ];
 
+  int _flagsToSet = 30;
+  int _seconds = 0;
+  bool _isRunning = false;
+
   void _openField(int row, int column) {
     setState(() {
+      if (!_isRunning) _startTimer();
       _state[row][column] = column % 8 + 1;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    List<Widget> tiles = [];
-    for (int index = 1; index <= 8; index++) {
-      tiles.add(new Tile(
-        id: index,
-        size: MediaQuery
-            .of(context)
-            .size
-            .width
-            .toDouble() / 10,
-      ),
-      );
-    }
-
     return new Scaffold(
       appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: new Text(widget.title),
       ),
       body: new Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          new Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Counter(
+                value: _flagsToSet,
+              ),
+              new Counter(
+                value: _seconds,
+              ),
+            ],
+          ),
           new Center(
-            // Center is a layout widget. It takes a single child and positions it
-            // in the middle of the parent.
             child: new Board(
               ids: _state,
               size: MediaQuery
@@ -109,7 +96,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   .width,
               onClick: _openField,
             ),
-          )
+          ),
         ],
       ),
     );
@@ -122,8 +109,7 @@ class Tile extends StatelessWidget {
   final double size;
   final VoidCallback onClick;
 
-
-  const Tile({this.id, this.size, this.onClick});
+  const Tile({Key key, this.id, this.size, this.onClick}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -138,7 +124,7 @@ class Tile extends StatelessWidget {
   }
 }
 
-typedef ColumnCallback(int);
+typedef void ColumnCallback(int);
 
 class TileRow extends StatelessWidget {
 
@@ -146,7 +132,8 @@ class TileRow extends StatelessWidget {
   final double width;
   final ColumnCallback onClick;
 
-  const TileRow({this.ids, this.width, this.onClick});
+  const TileRow({Key key, this.ids, this.width, this.onClick})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -166,7 +153,7 @@ class TileRow extends StatelessWidget {
   }
 }
 
-typedef RowColumnCallback(int row, int column);
+typedef void RowColumnCallback(int row, int column);
 
 class Board extends StatelessWidget {
 
@@ -190,6 +177,51 @@ class Board extends StatelessWidget {
     );
     return new Column(
         children: rows
+    );
+  }
+}
+
+class Digit extends StatelessWidget {
+
+  const Digit({Key key, this.value, this.size}) : super(key: key);
+
+  final int value;
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Image.asset(
+      'assets/images/counter_$value.gif',
+      fit: BoxFit.fill,
+      width: size.width,
+      height: size.height,
+    );
+  }
+}
+
+class Counter extends StatelessWidget {
+
+  const Counter({Key key, this.value}) : super(key: key);
+
+  final int value;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Row(
+      children: <Widget>[
+        new Digit(
+          value: (value / 100).floor() % 10,
+          size: new Size(26.0, 46.0),
+        ),
+        new Digit(
+          value: (value / 10).floor() % 10,
+          size: new Size(26.0, 46.0),
+        ),
+        new Digit(
+          value: value % 10,
+          size: new Size(26.0, 46.0),
+        ),
+      ],
     );
   }
 }
