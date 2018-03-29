87568b7859cdf8254a66fbebba0203f624ffa3a3
Volker Leck <volker.leck@gmail.com>
2018-03-29 18:15:03 +0200

    test for general screen setup

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,26 +4,27 @@
 // find child widgets in the widget tree, read text, and verify that the values of widget properties
 // are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
+import '../lib/board.dart';
+import '../lib/counter.dart';
 import '../lib/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Game has a board and two counters', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(new MinesApp());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.byType(Board), findsOneWidget);
+    expect(find.byType(Counter), findsNWidgets(2));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+//    // Tap the '+' icon and trigger a frame.
+//    await tester.tap(find.byIcon(Icons.add));
+//    await tester.pump();
+//
+//    // Verify that our counter has incremented.
+//    expect(find.text('0'), findsNothing);
+//    expect(find.text('1'), findsOneWidget);
   });
 }
