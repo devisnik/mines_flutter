71784cbeb88cf039a2d7d1de1ce52bdb00ca73ed
Volker Leck <volker.leck@gmail.com>
2018-03-29 17:21:21 +0200

    extract board into its own file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 
+import 'board.dart';
 import 'model.dart';
 
 void main() {
@@ -138,87 +139,6 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
-  }
-}
-
-class Tile extends StatelessWidget {
-  final int id;
-  final double size;
-  final VoidCallback onClick;
-  final VoidCallback onLongClick;
-
-  const Tile({Key key, this.id, this.size, this.onClick, this.onLongClick})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return new GestureDetector(
-        onTap: onClick,
-        onLongPress: onLongClick,
-        child: new Image.asset(
-          "assets/images/classic_image_${id < 10 ? "0$id" : "$id"}.png",
-          width: size,
-          height: size,
-          gaplessPlayback: true, //to avoid image flickering
-        ));
-  }
-}
-
-typedef void ColumnCallback(int);
-
-class TileRow extends StatelessWidget {
-  final List<int> ids;
-  final double width;
-  final ColumnCallback onClick;
-  final ColumnCallback onLongClick;
-
-  const TileRow({Key key, this.ids, this.width, this.onClick, this.onLongClick})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return new Row(
-        children: ids
-            .asMap()
-            .map((index, id) => new MapEntry(
-                index,
-                new Tile(
-                  id: id,
-                  size: width / ids.length,
-                  onClick: () => onClick(index),
-                  onLongClick: () => onLongClick(index),
-                )))
-            .values
-            .toList(growable: false));
-  }
-}
-
-typedef void RowColumnCallback(int row, int column);
-
-class Board extends StatelessWidget {
-  final List<List<int>> ids;
-  final double size;
-  final RowColumnCallback onClick;
-  final RowColumnCallback onLongClick;
-
-  const Board({this.ids, this.size, this.onClick, this.onLongClick});
-
-  @override
-  Widget build(BuildContext context) {
-    return new Column(
-        children: ids
-            .asMap()
-            .map((rowIndex, values) => new MapEntry(
-                rowIndex,
-                new TileRow(
-                  ids: values,
-                  width: size,
-                  onClick: (columnIndex) => onClick(rowIndex, columnIndex),
-                  onLongClick: (columnIndex) =>
-                      onLongClick(rowIndex, columnIndex),
-                )))
-            .values
-            .toList(growable: false));
   }
 }
 
